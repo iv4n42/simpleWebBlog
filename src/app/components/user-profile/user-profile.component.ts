a48e83e98ec80ff78af66c0906c17123cabed100
dmguezjaviersnet <dmguezjaviersnet@gmail.com>
2024-06-28 16:24:28 -0400

    Restructuring user profile page.

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
-  selector: 'app-user-page',
-  standalone: true,
-  imports: [MatTabsModule],
-  templateUrl: './user-profile.component.html',
-  styleUrl: './user-profile.component.scss'
+    selector: 'app-user-page',
+    standalone: true,
+    imports: [MatTabsModule, MatCardModule],
+    templateUrl: './user-profile.component.html',
+    styleUrl: './user-profile.component.scss',
 })
 export class UserProfileComponent {
-
+    onSelectProfilePicture() {
+    }
 }
