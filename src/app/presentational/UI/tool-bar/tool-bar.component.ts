c9225e192f77957c0c388cf895fd4488e9330af6
iv4n42 <ivanernand42@gmail.com>
2024-08-03 10:29:14 -0400

    Adding the search funtionality

--- src/app/presentational/UI/tool-bar/tool-bar.component.ts
+++ src/app/presentational/UI/tool-bar/tool-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -6,6 +6,9 @@ import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { SearchService } from '../../../shared/services/search/search.service';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-tool-bar',
@@ -18,16 +21,31 @@ import { RouterModule } from '@angular/router';
         MatButtonModule,
         RouterModule,
         CommonModule,
+        MatAutocompleteModule,
+        ReactiveFormsModule
     ],
     templateUrl: './tool-bar.component.html',
     styleUrl: './tool-bar.component.scss',
 })
-export class ToolBarComponent {
+export class ToolBarComponent implements OnInit {
     @Input() loggedIn!: boolean;
     @Input() sidenavOpened!: boolean;
 
     @Output() toggleSidenavEvent: EventEmitter<void> = new EventEmitter();
     @Output() logoutEvent: EventEmitter<void> = new EventEmitter();
+    searchForm! : FormGroup;
+    searchOptions! : string[];
+    constructor( public _searchService : SearchService){}
+
+    ngOnInit(): void {
+    this.searchForm = new FormGroup({
+        search : new FormControl("")
+    });
+    this._searchService.allOptions$.subscribe(options => {
+        this.searchOptions = options
+    });
+        
+    }
 
     onMenuIconBtnClick(): void {
         this.toggleSidenavEvent.emit();
@@ -35,5 +53,15 @@ export class ToolBarComponent {
 
     onLogoutBtnClick() {
         this.logoutEvent.emit();
+    }
+
+    submitSearch(){
+        this._searchService.finalOption$.next(this.searchForm.get("search")?.value);
+    }
+
+    filterOptions() {
+        let newOptions = this.searchOptions.filter( item => item.includes(this.searchForm.get("search")?.value));
+        this._searchService.searchOptions$.next(newOptions);
+       
     }
 }
--- src/app/presentational/post-listing/post-listing.component.ts
+++ src/app/presentational/post-listing/post-listing.component.ts
@@ -5,11 +5,12 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { HttpClient } from '@angular/common/http';
 import { UserService } from '../../shared/services/domain/user/user.service';
-import { Observable, Subject, switchMap, takeUntil } from 'rxjs';
+import { Observable, Subject, Subscription, switchMap, takeUntil } from 'rxjs';
 import { Post } from '../../shared/models/domain/post/Post';
 import { PostService } from '../../shared/services/domain/post/post.service';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { SearchService } from '../../shared/services/search/search.service';
 
 @Component({
     selector: 'app-post-page',
@@ -26,12 +27,12 @@ import { Router } from '@angular/router';
 })
 export class PostListingComponent implements OnInit, OnDestroy {
     private _destroy$: Subject<void> = new Subject();
+    filteredPosts? : Post[];
+    posts? : Post[];
+    searchSub! : Subscription;
 
-    posts$!: Observable<Post[]>;
-
-    constructor(private _postService: PostService, private _router: Router) {
-        this.posts$ = this._postService.getAllPosts();
-    }
+    constructor(private _postService: PostService, private _router: Router,
+                private _searchService : SearchService) {}
 
     ngOnInit() {
         this._postService.getPostValues.subscribe({
@@ -39,11 +40,28 @@ export class PostListingComponent implements OnInit, OnDestroy {
                 this._router.navigate([`post-read/${postId}`]);
             },
         });
+        this._postService.getAllPosts().subscribe(posts => {
+            this.posts = posts
+            this.filteredPosts = this.posts
+            let titles = []
+            for( let post of posts){
+                titles.push(post.title)
+            }
+            this._searchService.searchOptions$.next(titles)
+            this._searchService.allOptions$.next(titles)
+        });
+        this.searchSub = this._searchService.finalOption$.subscribe(title => {
+            this.filteredPosts = this.posts?.filter( post => post.title.includes(title))
+        });
     }
 
     ngOnDestroy(): void {
         this._destroy$.next();
         this._destroy$.complete();
+        this.searchSub.unsubscribe();
+        this._searchService.finalOption$.next("");
+        this._searchService.allOptions$.next([]);
+        this._searchService.searchOptions$.next([]);    
     }
 
     onPostCardClick(postId: string) {
